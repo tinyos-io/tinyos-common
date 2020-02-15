67c67b1c1b4eeaf66161d4c8258991cc3080d7d3
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2020-02-15 10:43:46 +0100

    arduino patch ok -> blink works

--- patchs/make_things_works_patch.sh
+++ patchs/make_things_works_patch.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+
+
+
+
+############################################################################## -- mega2560
 sed -i '/^PFLAGS += -mmcu=atmega2560.*/a\PFLAGS += -D__AVR_DEV_LIB_NAME__=m2560' src/tinyos-main/support/make/platforms/mega2560.platform
 
 
+sed -i '/^TARGET = mega2560.*/a\
+PROGRAMMER ?= avrdude\
+ifeq ($(PROGRAMMER),avrdude)\
+  PROGRAMMER_PART ?= -pm2560 #-U efuse:w:0xff:m\
+endif' src/tinyos-main/support/make/targets/mega2560.target
+
+
+cp ./common/patchs/files/m2560.extra src/tinyos-main/support/make/avr/
