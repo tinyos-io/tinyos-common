678b292180374c94b1fe4181330448e3a450bd11
abdullatifmouhamadi <abdullatif.mouhamadi@gmail.com>
2020-02-16 18:20:07 +0100

    enabling serial for mega2560

--- patchs/make_things_works_patch.sh
+++ patchs/make_things_works_patch.sh
@@ -6,7 +6,10 @@
 
 ############################################################################## -- mega2560
 sed -i '/^PFLAGS += -mmcu=atmega2560.*/a\PFLAGS += -D__AVR_DEV_LIB_NAME__=m2560' src/tinyos-main/support/make/platforms/mega2560.platform
+sed -i '/^PFLAGS += -I%T\/lib\/timer.*/a\PFLAGS += -I%T\/lib\/serial' src/tinyos-main/support/make/platforms/mega2560.platform
 
+
+############################################################################## -- 
 
 sed -i '/^TARGET = mega2560.*/a\
 PROGRAMMER ?= avrdude\
@@ -15,4 +18,11 @@ ifeq ($(PROGRAMMER),avrdude)\
 endif' src/tinyos-main/support/make/targets/mega2560.target
 
 
+
+
 cp ./common/patchs/files/m2560.extra src/tinyos-main/support/make/avr/
+cp ./common/patchs/files/platform_message.h src/tinyos-main/tos/platforms/mega2560
+cp ./common/patchs/files/PlatformSerialC.nc src/tinyos-main/tos/platforms/mega2560
+
+
+
